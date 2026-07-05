ac5a858d7e89e260fba6d355472498ba296bffab
priyenzi69-ux <priyenzi69@gmail.com>
2026-07-05 22:05:58 +0530

    Create subtraction.c

---
+++ subtraction.c
@@ -0,0 +1,13 @@
+// onine compiler for c
+#include <stdio.h>
+int main()
+{
+  int a, b, sub;
+printf("Enter first number:");
+scanf("%d", &a);
+printf("Enter second number:");
+scanf("%d", &b);
+sub = a - b;
+printf("subtraction = %d", sub);
+return 0;
+}
