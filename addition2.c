85ac811ff99a04a7963f4b722c99f9221c64c5de
priyenzi69-ux <priyenzi69@gmail.com>
2026-07-05 22:00:23 +0530

    Create addition2.c

---
+++ addition2.c
@@ -0,0 +1,13 @@
+// online program for c
+#include <stdio.h>
+int main()
+{
+  int a, b, sum;
+printf("Enter first number");
+scanf("%d", &a);
+printf("Enter second number");
+scanf("%d", &b);
+sum = a +b;
+printf("sum = %d");
+return 0 ;
+}
