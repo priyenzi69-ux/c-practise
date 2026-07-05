c660876d51e21342029b8da7fb099b87355d0599
priyenzi69-ux <priyenzi69@gmail.com>
2026-07-05 22:30:43 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,14 @@
+// Program to multiply two numbers
+#include <stdio.h>
+int main()
+{
+  int a, b, mul;
+printf(" Enter first number:");
+scanf("%d", &a);
+
+printf("Enter second number:");
+scanf("%d", &b);
+mul = a * b;
+printf("Multiplication = %d\n",mul);
+return 0 ;
+}
