9edbde7a4928d5fb70f6bfbce034b001c76e625a
priyenzi69-ux <priyenzi69@gmail.com>
2026-07-06 22:37:52 +0530

    Create turboc_add.c

---
+++ turboc_add.c
@@ -0,0 +1,17 @@
+*/Program to find sum of two numbers*/
+  #include <stdio.h>
+  #include <conio.h>
+  void main()
+{
+int a, b, sum;
+clrscr();
+printf("\n Enter the value of a=);
+  scanf("%d", &a);
+printf("\n Enter the value of b=);
+  scanf("%d", &b);
+sum = a + b;
+printf('\n value of sum = %d; sum);
+getch();
+}
+
+  
