2171d86c778a10c4b6615ed4beed0aee45a8f9ca
priyenzi69-ux <priyenzi69@gmail.com>
2026-07-11 23:18:37 +0530

    Create datatype.c

---
+++ datatype.c
@@ -0,0 +1,21 @@
+/*Program of float datatype of a and b*/
+# include <stdio.h>
+# include <conio.h>
+void main()
+{
+int a, b, mod;
+float sum, sub, mul, div;
+clrscr();
+printf ("Enter the value of a and b=");
+scanf("%d %d, &a &b);
+sum = a + b;
+sub = a - b;
+mul = a * b;
+div = a / b;
+mod = a % b;
+printf("\n sum = %f, sum);
+printf("\n sub = %f, sub);
+printf("\n mul = %f, mul);
+printf("\n div = %f, div);
+getch();
+}
