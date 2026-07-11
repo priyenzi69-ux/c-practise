c7e7f986f53dee47911c3a903c3c707a2b4d7853
Priyanshi <priyenzi69@gmail.com>
2026-07-12 00:32:24 +0530

    Create datatypea,b,x,y.c

---
+++ datatypea,b,x,y.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <conio.h>
+void main()
+{
+int a, b;
+long int x, y;
+unsigned int i;
+unsigned long int j;
+clrscr();
+printf("Enter the value of a, b, x, y");
+scanf("%d%d%ld%ld",&a, &b, &x, &y);
+printf("\n%d%d%ld%ld", a, b, x, y);
+scanf("%lu%lu",i ,j);
+printf("/n%u%lu",i, j);
+getch();
+}
