5130d85cc65c8856e16af8e099a9a65808280ab1
Priyanshi <priyenzi69@gmail.com>
2026-07-17 23:19:51 +0530

    Create datatypedldlu.c

---
+++ datatypedldlu.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include <conio.h>
+void main ()
+{
+  int a, b;
+long int x,y;
+unsigned int i;
+unsigned long int j;
+clrscrr();
+printf(" Enter the value of a, b, x, y");
+scanf("%d%d%ld%ld, &a, *b, &x, &y");
+printf("\n %d%d%ld%ld",a,b,x,y);
+scanf("%lu%lu", &i &j);
+printf("\n %lu%lu", i, j);
+getch()
+}
+  
